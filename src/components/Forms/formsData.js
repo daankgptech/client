b1b116857881223ac55d3869a0141a0ffa546427
shani8828 <shani.maurya.iitkgp@gmail.com>
2025-10-09 18:08:37 +0530

    count error fix

--- src/components/Forms/formsData.js
+++ src/components/Forms/formsData.js
@@ -18,11 +18,11 @@ const formsData = [
         deadline:"2025-10-05T23:59:00+05:30",
         isExceeded:false,
         responsePercentage:[
-            { batch:"'25",percentage:3/42 },
-            { batch:"'24",percentage:4/46 },
-            { batch:"'23",percentage:0/35 },
-            { batch:"'22",percentage:0/32 },
-            { batch:"'21",percentage:0/17 },
+            { batch:"'25",percentage:(3/42)*100 },
+            { batch:"'24",percentage:(4/46)*100 },
+            { batch:"'23",percentage:(0/35)*100 },
+            { batch:"'22",percentage:(0/32)*100 },
+            { batch:"'21",percentage:(0/17)*100 },
         ]
     },
     {
@@ -33,11 +33,11 @@ const formsData = [
         deadline:"2025-09-23T23:59:00+05:30",
         isExceeded:true,
         responsePercentage:[
-            { batch:"'25",percentage:95.12 },
-            { batch:"'24",percentage:77.27 },
-            { batch:"'23",percentage:85.71 },
-            { batch:"'22",percentage:78.12 },
-            { batch:"'21",percentage:72.72 },
+            { batch:"'25",percentage:(39/41)*100 },
+            { batch:"'24",percentage:(34/46)*100 },
+            { batch:"'23",percentage:(30/35)*100 },
+            { batch:"'22",percentage:(24/32)*100 },
+            { batch:"'21",percentage:(8/17 )*100},
         ]
     },
     

6ab1d8ec822e0450655ea88d190b14324710f548
shani8828 <shani.maurya.iitkgp@gmail.com>
2025-11-08 09:40:20 +0530

    forms Response Tech team updated

--- src/components/Forms/formsData.js
+++ src/components/Forms/formsData.js
@@ -1,73 +1,72 @@
 const formsData = [
-    {
-        to:"/forms/tech-team",
-        img:"https://res.cloudinary.com/dcwwptwzt/image/upload/v1762128970/TechTeam_oqptaq.avif",
-        title:"Tech Team Form",
-        desc:"Join the minds behind DAAN KGP. Build, innovate, and grow with DAAN KGP Tech Team!",
-        deadline:"2025-11-06T23:59:00+05:30",
-        isExceeded:false,
-        responsePercentage:[
-            { batch:"'25",percentage:(0/42)*100 },
-        ]
-    },
-    {
-        to:"/forms/feature",
-        img:"https://res.cloudinary.com/dcwwptwzt/image/upload/v1761065738/FeatureSuggestion_jhxar7.avif",
-        title:"Feature Suggestion Form",
-        desc:"Suggest new features, improvements, or ideas for the DAAN KGP platform.",
-        deadline:"2025-11-30T23:59:00+05:30",
-        isExceeded:false,
-        responsePercentage:[
-            { batch:"'25",percentage:(0/42)*100 },
-            { batch:"'24",percentage:(0/46)*100 },
-            { batch:"'23",percentage:(0/35)*100 },
-            { batch:"'22",percentage:(0/32)*100 },
-            { batch:"'21",percentage:(0/17)*100 },
-        ]
-    },
-    {
-        to:"/forms/data-update",
-        img:"https://res.cloudinary.com/dcwwptwzt/image/upload/v1759526380/data-update_wvwslm.avif",
-        title:"Data Updation Form",
-        desc:"Wanna rewrite infos over website! Update here.",
-        deadline:"2025-10-31T23:59:00+05:30",
-        isExceeded:true,
-        responsePercentage:[
-            { batch:"'25",percentage:(7/42)*100 },
-            { batch:"'24",percentage:(10/46)*100 },
-            { batch:"'23",percentage:(4/35)*100 },
-            { batch:"'22",percentage:(5/32)*100 },
-            { batch:"'21",percentage:(0/17)*100 },
-        ]
-    },
-    {
-        to:"/forms/cake-design-competition",
-        img:"https://res.cloudinary.com/dcwwptwzt/image/upload/v1759532933/cakeDesignCompetition_cxxwxs.avif",
-        title:"Cake Design Competition - 25",
-        desc:"Show your creativity in the Dakshana Day Cake Design Competition 2025!",
-        deadline:"2025-10-12T23:59:00+05:30",
-        isExceeded:true,
-        responsePercentage:[
-            { batch:"'25",percentage:(10/42)*100 },
-            { batch:"'24",percentage:(4/42)*100 },
-        ]
-    },
-    {
-        to:"/forms/tshirt",
-        img:"https://res.cloudinary.com/dcwwptwzt/image/upload/v1758206325/tshirt-form_g8yz8e.avif",
-        title:"T-Shirt Registration 2025",
-        desc:"Your Dakshana T-shirt is waiting! Register now and claim yours.",
-        deadline:"2025-09-23T23:59:00+05:30",
-        isExceeded:true,
-        responsePercentage:[
-            { batch:"'25",percentage:(39/41)*100 },
-            { batch:"'24",percentage:(34/46)*100 },
-            { batch:"'23",percentage:(30/35)*100 },
-            { batch:"'22",percentage:(24/32)*100 },
-            { batch:"'21",percentage:(7/9 )*100},
-        ]
-    },
-    
-]
+  {
+    to: "/forms/feature",
+    img: "https://res.cloudinary.com/dcwwptwzt/image/upload/v1761065738/FeatureSuggestion_jhxar7.avif",
+    title: "Feature Suggestion Form",
+    desc: "Suggest new features, improvements, or ideas for the DAAN KGP platform.",
+    deadline: "2025-11-30T23:59:00+05:30",
+    isExceeded: false,
+    responsePercentage: [
+      { batch: "'25", percentage: (0 / 42) * 100 },
+      { batch: "'24", percentage: (0 / 46) * 100 },
+      { batch: "'23", percentage: (0 / 35) * 100 },
+      { batch: "'22", percentage: (0 / 32) * 100 },
+      { batch: "'21", percentage: (0 / 17) * 100 },
+    ],
+  },
+  {
+    to: "/forms/tech-team",
+    img: "https://res.cloudinary.com/dcwwptwzt/image/upload/v1762128970/TechTeam_oqptaq.avif",
+    title: "Tech Team Form",
+    desc: "Join the minds behind DAAN KGP. Build and grow with DAAN KGP Tech Team!",
+    deadline: "2025-11-06T23:59:00+05:30",
+    isExceeded: true,
+    responsePercentage: [
+      { batch: "'25", percentage: (10 / 42) * 100 },
+      { batch: "'24", percentage: (3 / 46) * 100 },
+    ],
+  },
+  {
+    to: "/forms/data-update",
+    img: "https://res.cloudinary.com/dcwwptwzt/image/upload/v1759526380/data-update_wvwslm.avif",
+    title: "Data Updation Form",
+    desc: "Wanna rewrite infos over website! Update here.",
+    deadline: "2025-10-31T23:59:00+05:30",
+    isExceeded: true,
+    responsePercentage: [
+      { batch: "'25", percentage: (7 / 42) * 100 },
+      { batch: "'24", percentage: (10 / 46) * 100 },
+      { batch: "'23", percentage: (4 / 35) * 100 },
+      { batch: "'22", percentage: (5 / 32) * 100 },
+      { batch: "'21", percentage: (0 / 17) * 100 },
+    ],
+  },
+  {
+    to: "/forms/cake-design-competition",
+    img: "https://res.cloudinary.com/dcwwptwzt/image/upload/v1759532933/cakeDesignCompetition_cxxwxs.avif",
+    title: "Cake Design Competition - 25",
+    desc: "Show your creativity in the Dakshana Day Cake Design Competition 2025!",
+    deadline: "2025-10-12T23:59:00+05:30",
+    isExceeded: true,
+    responsePercentage: [
+      { batch: "'25", percentage: (10 / 42) * 100 },
+      { batch: "'24", percentage: (4 / 42) * 100 },
+    ],
+  },
+  {
+    to: "/forms/tshirt",
+    img: "https://res.cloudinary.com/dcwwptwzt/image/upload/v1758206325/tshirt-form_g8yz8e.avif",
+    title: "T-Shirt Registration 2025",
+    desc: "Your Dakshana T-shirt is waiting! Register now and claim yours.",
+    deadline: "2025-09-23T23:59:00+05:30",
+    isExceeded: true,
+    responsePercentage: [
+      { batch: "'25", percentage: (39 / 41) * 100 },
+      { batch: "'24", percentage: (34 / 46) * 100 },
+      { batch: "'23", percentage: (30 / 35) * 100 },
+      { batch: "'22", percentage: (24 / 32) * 100 },
+      { batch: "'21", percentage: (7 / 9) * 100 },
+    ],
+  },
+];
 export default formsData;
-

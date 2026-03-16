c46eb30d04314e9f0f5ae828165add278fcf51e5
shani8828 <shani.maurya.iitkgp@gmail.com>
2026-03-17 01:29:29 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ import Footer from "./components/Footer/Footer";
 import PageUpBtn from "./utils/PageUpBtn";
 import GlobalClickSpark from "./components/ClickEffect/GlobalClickSpark";
 
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import Feature from "./components/Forms/Feature";
 import FamCardDetails from "./components/OurFam/FamCardDetails";

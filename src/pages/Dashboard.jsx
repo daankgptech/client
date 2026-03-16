c8991d3de8c612bdd7685f08a2bbeb2cf323a213
shani8828 <shani.maurya.iitkgp@gmail.com>
2026-03-17 01:37:28 +0530

    removed header from dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -56,33 +56,28 @@ Access academic insights, task management, weather updates, motivational content
 
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-[#0f172a] text-slate-900 dark:text-slate-200 p-4 md:p-8 lg:p-12 transition-colors duration-500 container">
-      <Helmet>
-        {/* Standard metadata */}
+      {/* <Helmet>
         <title>{pageTitle}</title>
         <meta name="description" content={pageDescription} />
         <meta
           name="keywords"
           content="DAAN KGP, Dakshana Foundation, Dakshana Alumni Network, Dakshana IIT Kharagpur, Dakshana scholars IIT KGP, IIT Kharagpur alumni network, DAAN IIT Kharagpur, student mentorship IIT Kharagpur, career guidance Dakshana alumni, higher studies guidance IIT KGP, alumni mentorship programs IIT, student support Dakshana scholars, Dakshana community Kharagpur, alumni-student connect IIT KGP, networking for Dakshana alumni, IIT Kharagpur student-alumni network, collaboration among Dakshana scholars, social initiatives Dakshana alumni, outreach programs IIT KGP, volunteering at IIT Kharagpur, giving back to society IIT alumni, awareness campaigns by DAAN, how Dakshana alumni help IIT Kharagpur students, mentorship opportunities for Dakshana scholars, alumni guidance network at IIT Kharagpur, career counseling by Dakshana alumni, Dakshana student community at IIT KGP, daan kgp, daan-kgp, kgpian dakshanite, dakshanites at kgp, dakshanites at iit kgp, kgpian dakshanites, dakshana alumni network at Indian institute of technology Kharagpur, daan at iit kgp, daan at kgp, kgp dakshana, dakshana, iitkgp, kgp, kharagpur"
         />
-        <link rel="canonical" href="https://daan-kgp.vercel.app/dashboard" />
-        {/* Open Graph / Facebook / LinkedIn */}
+        <link rel="canonical" href="https://daankgp.vercel.app/dashboard" />
         <meta property="og:type" content="website" />
         <meta
           property="og:url"
-          content="https://daan-kgp.vercel.app/dashboard"
+          content="https://daankgp.vercel.app/dashboard"
         />
         <meta property="og:title" content={pageTitle} />
         <meta property="og:description" content={pageDescription} />
         <meta
           property="og:image"
           content="https://res.cloudinary.com/dhv0sckmq/image/upload/v1769529398/Logo_NoBG_op55cy.avif"
-        />{" "}
-        {/* Add a real path to your logo/banner */}
-        {/* The Thumbnail Image - This is what shows up in the WhatsApp chat bubble */}
+        />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
-        {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={pageTitle} />
         <meta name="twitter:description" content={pageDescription} />
@@ -90,7 +85,7 @@ Access academic insights, task management, weather updates, motivational content
           name="twitter:image"
           content="https://res.cloudinary.com/dhv0sckmq/image/upload/v1769529398/Logo_NoBG_op55cy.avif"
         />
-      </Helmet>
+      </Helmet> */}
       {/* HEADER */}
       <header className="max-w-[1600px] mx-auto mb-10">
         <div className="grid grid-cols-2 lg:grid-cols-4 items-start gap-6">
